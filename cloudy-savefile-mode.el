62d45d8db19e195fe6d418d5e4a84be611ff6309
William Henney <will@henney.org>
2018-11-13 13:48:15 -0600

    Add minimal header

--- cloudy-savefile-mode.el
+++ cloudy-savefile-mode.el
@@ -1,3 +1,51 @@
+;;; cloudy-savefile-mode.el --- 
+;; 
+;; Filename: cloudy-savefile-mode.el
+;; Description: Major mode for viewing Cloudy save files
+;; Author: William Henney
+;; Maintainer: William Henney
+;; Created: Tue Nov 13 13:42:38 2018 (-0600)
+;; Version: 0.1
+;; Package-Requires: (csv-mode)
+;; Last-Updated: Tue Nov 13 13:43:47 CST 2018
+;;           By: William Henney
+;;     Update #: 0
+;; URL: 
+;; Doc URL: 
+;; Keywords: 
+;; Compatibility: 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Commentary: 
+;; 
+;; See cloudy-savefile-mode.org
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Change Log:
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Code:
+
+
 (define-derived-mode cloudy-savefile-mode
   csv-mode "Cloudy save file"
   "Major mode for viewing Cloudy save files"
@@ -20,3 +68,6 @@
              ;; Avoid wrapping - must be done in hook so as to undo
              ;; effects of text-mode-hook
              (setq truncate-lines t)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; cloudy-savefile-mode.el ends here
